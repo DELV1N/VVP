f480f55e11c577e9f14e9129065e7642293cdac3
Sargis Gevorgyan <gevorgyan_sak@mail.ru>
2020-10-29 19:27:33 +0300

    .

--- Лаб9_3/Лаб9_3.cpp
+++ Лаб9_3/Лаб9_3.cpp
@@ -4,13 +4,21 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	int K,N;
+	int K,N,a;
 	cout << "Введите день года и день с которого начался год \n";
 	cin >> K >> N;
 	if (K > 0 and K < 366 and N>0 and N < 8)
 	{
-		K = K % N;
-		cout << "Номер дня недели для этого года равен " <<K;
+		a = K % 7 + N - 1;
+		if (a == 0)
+		{
+			a == 7;
+		    cout << "Номер дня недели для этого года равен " << a;
+		}
+		else
+		{
+			cout << "Номер дня недели для этого года равен " << a;
+		}
 	}
 	else
 	{
